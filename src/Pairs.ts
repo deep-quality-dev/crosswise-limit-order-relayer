b655c1af4e5d28361f00e1e201e1dd01ba5a99a7
deep-quality-dev <hungrywarrior081@gmail.com>
2021-12-21 12:27:42 +0800

    pair details for crss, bnb

--- src/Pairs.ts
+++ src/Pairs.ts
@@ -1,6 +1,6 @@
 import {Fetcher, Pair, Token} from '@crosswise/sdk'
+import axios from 'axios'
 import {ethers} from 'ethers'
-import fetch from 'node-fetch'
 import TokenEntry, {toToken} from './types/TokenEntry'
 import PancakePairAbi from './abis/PancakePair.json'
 
@@ -25,11 +25,47 @@ class Pairs {
   }
 
   static async fetch(provider: ethers.providers.BaseProvider) {
-    const res = await fetch(
-      'https://tokens.pancakeswap.finance/pancakeswap-top-100.json'
-    )
-    const resJson: any = await res.json()
-    const tokens: TokenEntry[] = resJson.tokens
+    // const res = await axios.get(
+    //   'https://tokens.pancakeswap.finance/pancakeswap-top-100.json'
+    // )
+    // const resJson: any = await res.data
+    // const tokens: TokenEntry[] = resJson.tokens
+
+    const tokens: TokenEntry[] = [
+      {
+        chainId: 97,
+        address: '0xbD3079d92db300E4574692F5e4Fc5911a54B8057',
+        decimals: 18,
+        symbol: 'CRSS',
+        name: 'CRSS',
+        blockNumber: 0,
+      },
+      {
+        chainId: 97,
+        address: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+        decimals: 18,
+        symbol: 'BNB',
+        name: 'BNB',
+        blockNumber: 0,
+      },
+      {
+        chainId: 56,
+        address: '0x0999ba9aEA33DcA5B615fFc9F8f88D260eAB74F1',
+        decimals: 18,
+        symbol: 'CRSS',
+        name: 'CRSS',
+        blockNumber: 0,
+      },
+      {
+        chainId: 56,
+        address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+        decimals: 18,
+        symbol: 'BNB',
+        name: 'BNB',
+        blockNumber: 0,
+      },
+    ]
+
     const tokenCombinations: [Token, Token][] = []
     for (const entryA of tokens) {
       const tokenA = toToken(entryA)
